87b41ed50a07522706ac4b509d0925e67095e3cd
DeivideSouza <deivide111@gmail.com>
2025-02-19 15:22:49 -0300

    Feat: Criação Instanciando Pessoa Fisica

--- src/main/java/com/example/btalento/config/CorsConfig.java
+++ src/main/java/com/example/btalento/config/CorsConfig.java
@@ -15,10 +15,10 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") // Angular rodando na porta 4200
+                        .allowedOrigins("*") // Angular rodando na porta 4200
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
